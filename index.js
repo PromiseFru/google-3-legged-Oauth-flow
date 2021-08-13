9be7be05b8ca47cb91aa0eb0a4c9f884a4c77efb
Promise Fru <promisefru1@gmail.com>
2021-08-13 11:58:12 +0100

    get auth_url from /start

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
-var OAuth = require('oauth');
 var express = require("express");
+const {
+    google
+} = require('googleapis');
 
 const app = express();
 
@@ -9,24 +11,43 @@ let CLIENT_ID = process.env.CLIENT_ID;
 let CLIENT_SECRET = process.env.CLIENT_SECRET;
 let REDIRECT_URL = process.env.REDIRECT_URL;
 
+const oauth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URL
+);
+
+// generate a url that asks permissions for Blogger and Google Calendar scopes
+const scopes = [
+    'https://www.googleapis.com/auth/userinfo.profile'
+];
+
 // start oauth
-app.post("/start", (req, res, next) => {
-   
+app.get("/start", (req, res, next) => {
+    const url = oauth2Client.generateAuthUrl({
+        // 'online' (default) or 'offline' (gets refresh_token)
+        access_type: 'offline',
+
+        // If you only need one scope you can pass it as a string
+        scope: scopes
+    });
+
+    return res.status(200).json(url)
 });
 
 // oauth callback
 app.get("/callback", (req, res, next) => {
-  
+
 });
 
 // get users profile
 app.get("/user", (req, res, next) => {
-    
+
 })
 
 // revoke
 app.delete("/user", (req, res, next) => {
-   
+
 })
 
 app.listen(3000, console.log("Server running"));
